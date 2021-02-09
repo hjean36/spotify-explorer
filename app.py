cc3bf7710a189dead683bab4706e614445877f7c
hjean36 <hjean36@outlook.com>
2021-02-08 21:12:32 -0500

    remove webapp2

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
 import streamlit as st
 import pandas as pd
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('I update automatically!')
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
 
 def run():
     st.title("Demo App Updated")
