60219ad5db78c34487cbbcf81937ca67b5d136b0
hjean36 <hjean36@outlook.com>
2021-02-08 20:12:11 -0500

    Add cloud.yaml

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
+import webapp2
 
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('I update automatically!')
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
 
 def run():
     st.title("Demo App")
