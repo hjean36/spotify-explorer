21d8b52511870f557551a2377bfbac8d8be1deb5
hjean36 <hjean36@outlook.com>
2021-02-15 14:23:59 -0500

    Update Get Saved Tracks for Demo

--- app.py
+++ app.py
@@ -15,8 +15,14 @@ if __name__ == "__main__" :
     st.write('Saved Tracks')
     st.write(str(env_variables_set))
     sp = sp.generate_auth_sp('user-library-read')
+    if sp:
+        st.write('Authenticated Spotify')
     results = sp.current_user_saved_tracks(limit=10)
+    if results:
+        st.write('Retrieved Tracks')
     songs = [_process_tracks(item) for item in results['items']]
+    if songs:
+        st.write("Songs Processed")
     st.write(songs)
 
 
